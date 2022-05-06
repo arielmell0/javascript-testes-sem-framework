fe828049e54f34aa4cdf6db747ea4c3bdc1d0303
arielmell0 <arielmello@rede.ulbra.br>
2022-05-06 10:24:51 -0300

    feat: validando ao terminar e comentando a estrutura
    ao terminar o programa valida todas as chamadas e comentando a nossa estrutura para fins didaticos

--- index.test.js
+++ index.test.js
@@ -2,6 +2,11 @@ import assert from 'assert'
 import Product from './product.js'
 import Service from './service.js'
 
+const callTracker = new assert.CallTracker()
+
+// quando o programa terminar, valida todas as chamadas
+process.on('exit', () => callTracker.verify())
+
 // should thrown an error when description is less than 5 characters long
 {
     const params = {
@@ -24,16 +29,35 @@ import Service from './service.js'
 
 // should save product successfully 
 {
+    // MOCK => O que precisamos para o teste funcionar
     const params = {
         description: 'my product',
         id: 1,
         price: 1000
     }
 
+    // serviceStub = impedir que seja ONLINE
+    const spy = callTracker.calls(1)
+    const serviceStub = {
+        async save(params) {
+            // SPY: espiona a função
+            spy(params)
+            return `${params.id} saved with sucess!`
+        }
+    }
+    const fn = (msg) => {
+        assert.deepStrictEqual(msg.id, params.id, 'id should be the same')
+        assert.deepStrictEqual(msg.price, params.price, 'price should be the same')
+        assert.deepStrictEqual(msg.description, params.description.toUpperCase(), 'description should be the same')
+    }
+
+    const spyOnCreate = callTracker.calls(fn, 1) 
     const product = new Product({
-        onCreate: (msg) => console.log('chamou onCreate', msg),
-        service: new Service()
+        onCreate: spyOnCreate,
+        // aqui fizemos o STUB
+        service: serviceStub
     })
 
     const result = await product.create(params)
+    assert.deepStrictEqual(result, `${params.id} SAVED WITH SUCESS!`)
 }
